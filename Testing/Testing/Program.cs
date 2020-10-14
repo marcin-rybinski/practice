ad60564606b474446051cfed064e7580be93d2e2
Marcin Rybinski <rybinski.marcin@gmail.com>
2020-10-14 16:21:57 +0200

    linq-exercises and testing projects created

---
+++ Testing/Testing/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Dog();
+            var wolf = new Wolf();
+            Animal animal = new Animal();
+            int i = 0;
+            double f = 2.1;
+
+            //animal = wolf; //ok
+
+            //wolf = animal; //nie ok 
+
+            //wolf = dog; //nie ok
+
+            //wolf = (Wolf)dog; //nie ok
+
+            //wolf = (Wolf) (Animal) dog; //ok
+
+            //wolf = animal as Wolf; //ok
+
+            wolf = (Wolf) animal; //ok
+
+            i = (int)f;
+
+            //Console.Write("Enter something:");
+            //string s1 = Console.ReadLine();
+
+            //if (String.IsNullOrEmpty(s1))
+            //{
+            //    Console.WriteLine("You didn't enter anything!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Thank you for your submission!");
+            //}
+
+            //string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            //string[] heroes = { "D. Va", "Lucio", "Mercy", "Soldier 76", "Pharah", "Reinhardt" };
+
+            //var heroesT = heroes.Select(h => $"Introducing...{h}");
+
+            //var spacesI = heroes.Where(h => h.Contains(' '))
+            //    .Select(s => s.IndexOf(' ')).ToList();
+        }
+    }
+    class Animal { }
+    class Wolf : Animal { }
+    class Dog : Animal { }
+
+}
--- linq-exercises/Linq-exercises/Linq-exercises/UnitTest1.cs
+++ linq-exercises/Linq-exercises/Linq-exercises/UnitTest1.cs
@@ -1,14 +1,122 @@
 using System;
+using System.Linq;
+using FluentAssertions;
+using MoreLinq;
 using Xunit;
 
 namespace Linq_exercises
 {
-    public class UnitTest1
+    public class Challenges
     {
         [Fact]
-        public void Test1()
+        public void One()
         {
+            const string n = "1-5;15-17;25-30";
 
+            // TODO: Generate numbers e.g. 1-5;7-9 means 1, 2, 3, 4, 5, 7, 8, 9
+
+            var numbers = n.Split(';').Select(x => x.Split('-'))
+                .Select(x => x.Select(int.Parse))
+                .Select(x => MoreEnumerable.Sequence(x.Min(), x.Max()))
+                .Flatten().ToList();
+                
+
+            numbers.Should().Equal(1, 2, 3, 4, 5, 15, 16, 17, 25, 26, 27, 28, 29, 30);
+        }
+
+        [Fact]
+        public void Two()
+        {
+            var names = new[] { "Marian", "Bartek", "Beata", "Daniel", "Monika", "Damian" };
+
+            //scores of those who finished the game
+            var scores = new[] { 350, 550, 550, 450 };
+
+            //TODO: Get the best players (names and score)
+
+            //  bestPlayers.Should().Equal(new { name = "Bartek", score = 550 }, new { name = "Beata", score = 550 });
+        }
+
+        [Fact]
+        public void Three()
+        {
+            var students = new[]
+                {"Bartek", "Blazej", "Natalia", "Danuta", "Mariusz", "Marian", "Beata", "Kasia", "Basia"};
+
+            // TODO: assign students to groups of max. four persons.
+
+            //studentGroups.Should().BeEquivalentTo(
+            //    new
+            //    {
+            //        groupNumber = 1,
+            //        groupMembers = new[]
+            //        {
+            //            "Bartek", "Blazej", "Natalia", "Danuta"
+            //        }
+            //    },
+            //    new
+            //    {
+            //        groupNumber = 2,
+            //        groupMembers = new[]
+            //        {
+            //            "Mariusz", "Marian", "Beata", "Kasia"
+            //        }
+            //    },
+            //    new
+            //    {
+            //        groupNumber = 3,
+            //        groupMembers = new[]
+            //        {
+            //            "Basia"
+            //        }
+            //    }
+            //);
+        }
+
+        [Fact]
+        public void Four()
+        {
+            var fruitOrVegetables = new[]
+            {
+                "apple",
+                "orange",
+                "orange",
+                "carrot",
+                "apple",
+                "carrot",
+                "carrot",
+                null,
+                "cucumber",
+                "apple",
+                "cucumber",
+                "cucumber",
+                "cucumber"
+            };
+
+            // TODO: Get only those fruit with total number of min. 3.
+
+            var fruit = new[]
+            {
+                "apple",
+                "orange"
+            };
+
+            //  minimum3Fruit.Should().Equal("apple");
+        }
+
+        [Fact]
+        public void Five()
+        {
+            var names = new[] { "Marian", null, "Bartek", "Daniel" };
+
+            var scores = new[] { 350, 550, 550, 450 };
+
+            // TODO: return the short summary of the players and their corresponding scores.
+            // Please ues the pattern: FirstPlayer:FirstPlayerScore - SecondPlayer:SecondPlayerScore to format the final result.
+            // Use 'N/A' value when the player name is missing
+
+
+            //   playersSummary.Should().Be("Bartek:550 - N/A:550 - Daniel:450 - Marian:350");
         }
     }
 }
